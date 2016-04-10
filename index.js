07078ff7b0b172a33eeaddee8d27dbeb52177e8f
doraki <dorajambor@gmail.com>
2016-04-09 20:20:25 -0400

    Freq mapped to mel scale

--- index.js
+++ index.js
@@ -14,62 +14,127 @@ navigator.mediaDevices.getUserMedia( {audio: true})
     .then((stream) => {
         let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-
         var source = audioCtx.createMediaStreamSource(stream);
-
-        // source.connect(audioCtx.destination);
-
-        console.log(source);
-
-        console.log(audioCtx);
 
         let analyser = audioCtx.createAnalyser();
         source.connect(analyser);
-        analyser.fftSize = 2048;
-        let bufferLength = analyser.frequencyBinCount;
-        console.log(bufferLength);
-        let dataArray = new Uint8Array(bufferLength);
-        canvasCtx.clearRect(0,0, WIDTH, HEIGHT);
 
+        analyser.fftSize = 2048;
+
+        let bufferLength = analyser.frequencyBinCount;
+        let dataArray = new Uint8Array(bufferLength);
+        const fftBinWidth = audioCtx.sampleRate / analyser.fftSize;
+
+        // console.log(fftBinWidth);  // 21
+
+        // mel scaling
+        function hzToMel(f) {
+            return 1127 * Math.log(1 + f/700)
+        }
+        function melToHz(f) {
+            return 700 * (Math.exp(f / 1127)-1)
+        }
+
+        function fromFFTRect(input) {
+            const numBins = 26;
+            const output = new Uint8Array(numBins);
+
+            const melWidth = hzToMel(audioCtx.sampleRate / 2) / numBins;
+
+            for (let i=0; i < output.length; i++){
+                let minHz = melToHz(i * melWidth);
+                let maxHz = melToHz((i + 1) * melWidth);
+
+                let minInputIndex = Math.floor(minHz / fftBinWidth);
+                let maxInputIndex = Math.floor(maxHz / fftBinWidth);
+
+                for (let j = minInputIndex; j < maxInputIndex; j++) {
+                    output[i] += input[j];
+                }
+            }
+            return output;
+        }
+
+        function fromFFTtriangle(input) {
+            const numBins = 100;
+            const numPoints = numBins + 2;
+            const numSections = numPoints - 1;
+            const output = new Uint8Array(numBins);
+
+            const melWidth = hzToMel(audioCtx.sampleRate / 2) / numSections;
+
+            for (let i=0; i < output.length; i++){
+                let minHz = melToHz(i * melWidth);
+                let centerHz = melToHz((i + 1) * melWidth);
+                let maxHz = melToHz((i + 2) * melWidth);
+
+                let slopeAscending = 1/(centerHz - minHz);
+                let slopeDescending = 1/(maxHz - centerHz);
+
+                let minInputIndex = Math.floor(minHz / fftBinWidth);
+                let maxInputIndex = Math.floor(maxHz / fftBinWidth);
+
+                for (let j = minInputIndex; j < maxInputIndex; j++) {
+                    // input[j] * 1 is for rectangle
+                    // input[j] * weight constructs triangle
+                    let mult;
+                    let hzVal = j * fftBinWidth;
+                    if (hzVal < centerHz) {
+                        mult = (hzVal - minHz) * slopeAscending;
+                    } else {
+                        mult = 1 + (hzVal - centerHz) * slopeDescending;
+                    }
+                    output[i] += mult * input[j];
+                }
+            }
+            return output;
+        }
+        
+        canvasCtx.clearRect(0,0, WIDTH, HEIGHT);
+        
         const draw = () => {
             drawVisual = requestAnimationFrame(draw)
+            analyser.getByteFrequencyData(dataArray);
+
+            let melDataArray = fromFFTtriangle(dataArray);
+            
+            canvasCtx.fillStyle = 'rgb(166,85,95)';
             canvasCtx.fillStyle = "rgba(255,255,255, 0.15)";
             canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-            analyser.getByteFrequencyData(dataArray);
-            canvasCtx.fillStyle = 'rgb(166,85,95)';
 
-            let barWidth = (WIDTH/ bufferLength) * 2;
-            let r =  255;
-            let g = Math.sin(new Date() / 5000);
-            let b = Math.sin(new Date() / 4000);
-            if(g < 0 || b < 0) {
-                g = Math.abs(g);
-                b = Math.abs(b);
-            }
-            if(g < 0.5 || b < 0.5) {
-                g = g + 0.3;
-                b = b + 0.3;
-            }
-            if(g > 0.8 || b > 0.8) {
-                g = g - 0.2;
-                b = b - 0.2;
-            }
-            let R = r;
-            let G = Math.floor(g * 255);
-            let B = Math.floor(g * 255);
+            let barWidth = (WIDTH/ melDataArray.length);
+            // let r =  255;
+            // let g = Math.sin(new Date() / 5000);
+            // let b = Math.sin(new Date() / 4000);
+            // if(g < 0 || b < 0) {
+            //     g = Math.abs(g);
+            //     b = Math.abs(b);
+            // }
+            // if(g < 0.5 || b < 0.5) {
+            //     g = g + 0.3;
+            //     b = b + 0.3;
+            // }
+            // if(g > 0.8 || b > 0.8) {
+            //     g = g - 0.2;
+            //     b = b - 0.2;
+            // }
+            // let R = r;
+            // let G = Math.floor(g * 255);
+            // let B = Math.floor(g * 255);
 
-           let alphA = 0.8;
-            for(let i =0; i <bufferLength; i++) {
-                let barHeight = Math.exp(dataArray[i] / 30);
-                if(i < bufferLength/2) {
-                    R -= 1;
-              
-                }
-                let barColor = 'rgba(' + R + ',' + G + ','+ B + ','+ (alphA) + ')';
-                canvasCtx.fillStyle = barColor;
-                canvasCtx.fillRect((barWidth + 1)*i, HEIGHT-barHeight/2, Math.floor(barWidth + 3), barHeight);
-            }
-        };
+           // let alphA = 0.8;
+            canvasCtx.fillStyle = 'rgba(0,0,0,1)';
+            for(let i =0; i <melDataArray.length; i++) {
+                let barHeight = melDataArray[i] * 2;
+                // if(i < bufferLength/2) {
+                    // R -= 1;
+                // }
+                // let barColor = 'rgba(' + R + ',' + G + ','+ B + ','+ (alphA) + ')';
+                canvasCtx.fillRect((barWidth + 1)*i
+                                , Math.floor(HEIGHT-barHeight/2)
+                                , Math.floor(barWidth) + 2
+                                , Math.floor(barHeight));
+            }};
         draw();
     })
     .catch((err) => {console.log(err);});
